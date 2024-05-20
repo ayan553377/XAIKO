789f61037c5234aea2e19c99328b73da0d7b06a9
XAIKO <ayanchowdhury55505@gmail.com>
2024-05-20 17:32:13 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -65,12 +65,13 @@ module.exports = {
 
 
       const helpListImages = [
-        "https://i.imgur.com/3ap4QdB.png", // add image link here
-        "https://i.imgur.com/RyPor2K.png",
-        "https://i.imgur.com/Td6POTk.png",
-        "https://i.imgur.com/VAFyQ4y.png",
-        "https://i.imgur.com/3TARwmi.png",
-        // Add more image links as needed
+        "https://i.imgur.com/ZP0ilD1.jpeg", // don't change image
+        "https://i.imgur.com/h5LTRu3.jpeg",
+        "https://i.imgur.com/tj6dKu9.jpeg",
+        "https://i.imgur.com/4zC4JN9.jpeg",
+        "https://i.imgur.com/yBowyV0.jpeg",
+        "https://i.imgur.com/eQ4fyO3.jpeg",
+        // image link fixed bro🖤
       ];
 
 
