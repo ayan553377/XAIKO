ff0a54c4f5295328b1f8cf66d7e5b92303ebf8ec
XAIKO <ayanchowdhury55505@gmail.com>
2024-05-21 08:35:40 +0600

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -16,7 +16,7 @@ module.exports = {
  return message.reply({
  body: `Hello bro\n\nMy prefix is [ . ]\n\n______________________________\n\n𝗛𝗢𝗪 𝗧𝗢 𝗨𝗦𝗘?\nexample please type: .help - to view sure cmds\n.menu\n.info\n.owner\n.ping\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥: MR.AYAN ツ\n______________________________
 `,
-  attachment: await global.utils.getStreamFromURL("https://i.postimg.cc/Qxc3t9rz/download.gif")
+  attachment: await global.utils.getStreamFromURL("https://i.imgur.com/XPFuf9m.mp4")
  });
  }
  }
